b8f1c59743f2c6b04f63877e5f58703d2e88920a
Your Name <george.murgoci94@gmail.com>
2021-03-08 15:48:51 +0200

    doamne ajuta

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 //!show navigation
 let index = 1;
 const faqContent = document.querySelector('.container-faq ');
@@ -426,75 +427,60 @@ const minPrice = Math.min(...priceList)
 
 
 
-var inputLeft = document.getElementById("input-left");
-var inputRight = document.getElementById("input-right");
+const sliderFunction =function() {
+	noUiSlider.create(doubleHandleSlider, {
+		start: [ minPrice, maxPrice],
+		connect: true,
+		tooltips:false,
+		step: 10,
+		range: {
+			'min': [minPrice],
+			'max': [maxPrice]
+		},
+		format: {
+			to: function(value) {
+				return value;
+			},
+			from: function(value) {
+				return value;
+			}
+		},
+      
+	});
+	
+	doubleHandleSlider.noUiSlider.on('change', function( values, handle ) { 
 
-var thumbLeft = document.querySelector(".slider > .thumb.left");
-var thumbRight = document.querySelector(".slider > .thumb.right");
-var range = document.querySelector(".slider > .range");
+		var val = values[handle]; // 0 or 1
+		
+		if(handle) {
+			maxValInput.value = Math.round(val) ;
+		} else {
+            minValInput.value = Math.round(val);
+		}
+	});
 
-function setLeftValue() {
-	var _this = inputLeft,
-		min = parseInt(_this.min),
-		max = parseInt(_this.max);
+        minValInput.addEventListener('change', function () {
+            doubleHandleSlider.noUiSlider.set([this.value, null]
+               
+            );
+    });
+	
+        maxValInput.addEventListener('change', function () {
+    
+	doubleHandleSlider.noUiSlider.set([null, this.value]);
+        });
+      
+};
+sliderFunction();
 
-	_this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
-
-	var percent = ((_this.value - min) / (max - min)) * 100;
-
-	thumbLeft.style.left = percent + "%";
-	range.style.left = percent + "%";
-}
-setLeftValue();
-
-function setRightValue() {
-	var _this = inputRight,
-		min = parseInt(_this.min),
-		max = parseInt(_this.max);
-
-	_this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
-
-	var percent = ((_this.value - min) / (max - min)) * 100;
-
-	thumbRight.style.right = (100 - percent) + "%";
-	range.style.right = (100 - percent) + "%";
-}
-setRightValue();
-
-
-
-
-
-
-
-inputLeft.addEventListener("input", setLeftValue);
-inputRight.addEventListener("input", setRightValue);
-
-inputLeft.addEventListener("mouseover", function() {
-	thumbLeft.classList.add("hover");
-});
-inputLeft.addEventListener("mouseout", function() {
-	thumbLeft.classList.remove("hover");
-});
-inputLeft.addEventListener("mousedown", function() {
-	thumbLeft.classList.add("active");
-});
-inputLeft.addEventListener("mouseup", function() {
-	thumbLeft.classList.remove("active");
-});
-
-inputRight.addEventListener("mouseover", function() {
-	thumbRight.classList.add("hover");
-});
-inputRight.addEventListener("mouseout", function() {
-	thumbRight.classList.remove("hover");
-});
-inputRight.addEventListener("mousedown", function() {
-	thumbRight.classList.add("active");
-});
-inputRight.addEventListener("mouseup", function() {
-	thumbRight.classList.remove("active");
-});
+doubleHandleSlider.addEventListener("click", () => {
+    let minWantedPrice = Math.min(...doubleHandleSlider.noUiSlider.get([0, null]));
+    let maxWantedPrice = Math.max(...doubleHandleSlider.noUiSlider.get([null, 1]));
+    let priceRange = prices.filter(function (price) {
+     return  price.getAttribute("price")>minWantedPrice &&  price.getAttribute("price")<maxWantedPrice
+    })
+    console.log(priceRange);
+})
 
 
 
